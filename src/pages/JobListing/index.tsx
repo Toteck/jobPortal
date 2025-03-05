cede981858df175b59e564100fe23993f9e37e0d
mateus <mateusweslley14@gmail.com>
2025-03-05 18:16:49 -0300

    feat: implement job fetching functionality with session-based authentication

--- src/pages/JobListing/index.tsx
+++ src/pages/JobListing/index.tsx
@@ -1,4 +1,31 @@
+import { getJobs } from "@/api/apijobs";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 const JobListing = () => {
+  const { session } = useSession();
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      if (session) {
+        const supabaseAccessToken = await session.getToken({
+          template: "supabase",
+        });
+        if (supabaseAccessToken) {
+          console.log(supabaseAccessToken);
+          const data = await getJobs(supabaseAccessToken);
+          console.log(data);
+        } else {
+          console.log("Failed to get supabase access token.");
+        }
+      } else {
+        console.log("Session is not loaded yet.");
+      }
+    };
+
+    fetchJobs();
+  }, [session]);
+
   return <div>JobListing</div>;
 };
 
